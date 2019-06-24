9f41b370a5b432537bd32a02c665822b498d87ba
donnasvinis <dvsvinis@mac.com>
2019-06-24 16:58:31 -0400

    Working on StringUtils - Capitalize nth passes

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,9 +9,14 @@ import java.util.List;
  * @author leon on 09/12/2018.
  */
 public class StringUtils {
+
     public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
-        return null;
+
+        String result = str.substring(0,indexToCapitalize) + str.substring(indexToCapitalize,indexToCapitalize+1).toUpperCase()
+                + str.substring(indexToCapitalize+1);
+        return result;
     }
+
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
         return null;
