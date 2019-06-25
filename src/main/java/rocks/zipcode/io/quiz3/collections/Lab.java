95ca117f8d6ca2908fc36bda55fb5c781cc49acc
donnasvinis <dvsvinis@mac.com>
2019-06-24 22:46:18 -0400

    Collections passes

--- src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
@@ -4,21 +4,36 @@ package rocks.zipcode.io.quiz3.collections;
  * @author leon on 10/12/2018.
  */
 public class Lab {
+    private String labName;
+    private LabStatus labStatus;
+    private Boolean fork;
+
     public Lab() {
         this(null);
     }
 
     public Lab(String labName) {
+        this.labName = labName;
     }
 
     public String getName() {
-        return null;
+        return labName;
     }
 
     public void setStatus(LabStatus labStatus) {
+        this.labStatus = labStatus;
     }
 
     public LabStatus getStatus() {
-        return null;
+
+        return labStatus;
+    }
+
+    public Boolean getFork(){
+        return fork;
+    }
+
+    public void setFork(Boolean fork){
+        this.fork = fork;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
@@ -1,30 +1,63 @@
 package rocks.zipcode.io.quiz3.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author leon on 10/12/2018.
  */
 public class Student {
+    private List<Lab> labList;
 
     public Student() {
-        this(null);
+        labList = new ArrayList<>();
     }
 
     public Student(List<Lab> labs) {
+        this.labList = labs;
     }
 
     public Lab getLab(String labName) {
+
+        for (Lab lab : labList) {
+            if (lab.getName().equals(labName)) {
+                return lab;
+            }
+        }
         return null;
     }
 
-    public void setLabStatus(String labName, LabStatus labStatus) {
+    public void setLabStatus(String labName, LabStatus labStatus) throws UnsupportedOperationException {
+        Lab lab = getLab(labName);
+        if (lab == null) {
+            throw new UnsupportedOperationException();
+        }
+        lab.setStatus(labStatus);
     }
 
     public void forkLab(Lab lab) {
+        lab.setStatus(LabStatus.PENDING);
+        labList.add(lab);
     }
 
     public LabStatus getLabStatus(String labName) {
+        for (Lab lab : labList) {
+            if (lab.getName().equals(labName)) {
+                return lab.getStatus();
+            }
+        }
         return null;
     }
+
+    @Override
+    public String toString() {
+        String list = "";
+
+        for (int i = labList.size() - 1; i >= 0; i--) {
+            list += labList.get(i).getName() + " > " + labList.get(i).getStatus() + "\n";
+        }
+        list = list.substring(0, list.length() - 1);
+        return list;
+    }
 }
+
